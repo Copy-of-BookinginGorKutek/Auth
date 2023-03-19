bd85ef71c916b84266c20fd7c9fd2d7450aee2ef
Sasha Nabila Fortuna <sasha.nabila@ui.ac.id>
2023-03-19 14:17:05 +0700

    model for permission

--- src/main/java/com/b2/bookingingorkutek/model/ApplicationUserPermission.java
+++ src/main/java/com/b2/bookingingorkutek/model/ApplicationUserPermission.java
@@ -1,8 +1,19 @@
 package com.b2.bookingingorkutek.model;
 
 public enum ApplicationUserPermission {
+
+    BOOKING_READ_ALL(""),
+    BOOKING_READ_SELF(""),
+    BOOKING_CREATE(""),
+    BOOKING_UPDATE(""),
+    BOOKING_DELETE(""),
+    GOR_READ(""),
+    GOR_CREATE(""),
+    GOR_UPDATE(""),
+    GOR_DELETE(""),
     REGISTER("auth:register"),
     LOGIN("auth:login");
+
     private final String permission;
 
     ApplicationUserPermission(String permission) {
