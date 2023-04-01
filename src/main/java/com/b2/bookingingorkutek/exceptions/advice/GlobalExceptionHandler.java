672d5809a7234c645a6a2d7bd5ef0203d00f871b
stenly10 <yosuasaputra.stenly@gmail.com>
2023-04-01 22:07:21 +0700

    mengembalikan isi global exception handler

--- src/main/java/com/b2/bookingingorkutek/exceptions/advice/GlobalExceptionHandler.java
+++ src/main/java/com/b2/bookingingorkutek/exceptions/advice/GlobalExceptionHandler.java
@@ -1,59 +1,59 @@
-//package com.b2.bookingingorkutek.exceptions.advice;
-//
-//import com.b2.bookingingorkutek.exceptions.ErrorTemplate;
-//import com.b2.bookingingorkutek.exceptions.UserAlreadyExistException;
-//import io.jsonwebtoken.JwtException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//
-//import javax.naming.AuthenticationException;
-//import java.time.ZoneId;
-//import java.time.ZonedDateTime;
-//
-//@ControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    @ExceptionHandler(value = {UserAlreadyExistException.class})
-//    public ResponseEntity<Object> userExist() {
-//        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
-//        ErrorTemplate baseException = new ErrorTemplate(
-//                "User with the same email already exist",
-//                badRequest,
-//                ZonedDateTime.now(ZoneId.of("Z"))
-//        );
-//
-//        return new ResponseEntity<>(baseException, badRequest);
-//    }
-//
-//
-//    @ExceptionHandler(value = {JwtException.class, AuthenticationException.class, UsernameNotFoundException.class})
-//    public ResponseEntity<Object> credentialsError(Exception exception) {
-//        HttpStatus badRequest = HttpStatus.UNAUTHORIZED;
-//        ErrorTemplate baseException = new ErrorTemplate(
-//                exception.getMessage(),
-//                badRequest,
-//                ZonedDateTime.now(ZoneId.of("Z"))
-//        );
-//
-//        return new ResponseEntity<>(baseException, badRequest);
-//    }
-//
-//
-//    @ExceptionHandler(value = {Exception.class})
-//    public ResponseEntity<Object> generalError(Exception exception) {
-//        HttpStatus badRequest = HttpStatus.INTERNAL_SERVER_ERROR;
-//        ErrorTemplate baseException = new ErrorTemplate(
-//                exception.getMessage(),
-//                badRequest,
-//                ZonedDateTime.now(ZoneId.of("Z"))
-//        );
-//
-//        return new ResponseEntity<>(baseException, badRequest);
-//    }
-//
-//
-//}
-//
+package com.b2.bookingingorkutek.exceptions.advice;
+
+import com.b2.bookingingorkutek.exceptions.ErrorTemplate;
+import com.b2.bookingingorkutek.exceptions.UserAlreadyExistException;
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.naming.AuthenticationException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {UserAlreadyExistException.class})
+    public ResponseEntity<Object> userExist() {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        ErrorTemplate baseException = new ErrorTemplate(
+                "User with the same email already exist",
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(baseException, badRequest);
+    }
+
+
+    @ExceptionHandler(value = {JwtException.class, AuthenticationException.class, UsernameNotFoundException.class})
+    public ResponseEntity<Object> credentialsError(Exception exception) {
+        HttpStatus badRequest = HttpStatus.UNAUTHORIZED;
+        ErrorTemplate baseException = new ErrorTemplate(
+                exception.getMessage(),
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(baseException, badRequest);
+    }
+
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> generalError(Exception exception) {
+        HttpStatus badRequest = HttpStatus.INTERNAL_SERVER_ERROR;
+        ErrorTemplate baseException = new ErrorTemplate(
+                exception.getMessage(),
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(baseException, badRequest);
+    }
+
+
+}
+
