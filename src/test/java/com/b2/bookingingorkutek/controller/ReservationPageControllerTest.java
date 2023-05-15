8355d3dbbf34552ec31f743cff94806e0fdff8be
stenly10 <yosuasaputra.stenly@gmail.com>
2023-05-15 14:00:25 +0700

    progress perbaikan test

--- src/test/java/com/b2/bookingingorkutek/controller/ReservationPageControllerTest.java
+++ src/test/java/com/b2/bookingingorkutek/controller/ReservationPageControllerTest.java
@@ -1,24 +1,24 @@
 package com.b2.bookingingorkutek.controller;
 
-
-import com.b2.bookingingorkutek.dto.ModelUserDto;
-import com.b2.bookingingorkutek.service.AuthorizationService;
-import com.b2.bookingingorkutek.service.JwtService;
-import org.junit.jupiter.api.BeforeEach;
+//
+//import com.b2.bookingingorkutek.dto.ModelUserDto;
+//import com.b2.bookingingorkutek.service.AuthorizationService;
+//import com.b2.bookingingorkutek.service.JwtService;
+//import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.*;
-import org.springframework.test.web.servlet.MockMvc;
+//import org.mockito.InjectMocks;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.*;
+//import org.springframework.test.web.servlet.MockMvc;
 //import org.springframework.web.client.RestTemplate;
-
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import static org.mockito.Mockito.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //@WebMvcTest(controllers = ReservationPageController.class)
 //@AutoConfigureMockMvc(addFilters = false)
