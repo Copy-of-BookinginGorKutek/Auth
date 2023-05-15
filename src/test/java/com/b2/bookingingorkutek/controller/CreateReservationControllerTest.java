e0ede20ef53a808069ea995bc5130039ba1ee677
stenly10 <yosuasaputra.stenly@gmail.com>
2023-05-15 14:33:52 +0700

    progress perbaikan test

--- src/test/java/com/b2/bookingingorkutek/controller/CreateReservationControllerTest.java
+++ src/test/java/com/b2/bookingingorkutek/controller/CreateReservationControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -23,27 +23,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-//@WebMvcTest(controllers = CreateReservationController.class)
-//@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(controllers = CreateReservationController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class CreateReservationControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private JwtService jwtService;
+    private ModelUserDto modelUserDto;
+    @MockBean
+    private AuthorizationService authorizationService;
+    @MockBean
+    private RestTemplate restTemplate;
     @Test
-    void test(){}
-//    @Autowired
-//    private MockMvc mvc;
-//    @MockBean
-//    private JwtService jwtService;
-//    private ModelUserDto modelUserDto;
-//    @MockBean
-//    private AuthorizationService authorizationService;
-//    @MockBean
-//    private RestTemplate restTemplate;
-//    @Test
-//    void testAccessCreateReservation() throws Exception{
-//        when(restTemplate.postForEntity(anyString(), any(), any())).thenReturn(new ResponseEntity<>(null, HttpStatusCode.valueOf(200)));
-//        mvc.perform(post("/create-reservation/create")
-//                        .content("{}")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(handler().methodName("createReservationPost"));
-//    }
+    void testAccessCreateReservation() throws Exception{
+        when(restTemplate.postForEntity(anyString(), any(), any())).thenReturn(new ResponseEntity<>(null, HttpStatusCode.valueOf(200)));
+        mvc.perform(post("/create-reservation/create")
+                        .content("{}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("createReservationPost"));
+    }
 }
--- src/test/java/com/b2/bookingingorkutek/controller/ReservationPageControllerTest.java
+++ src/test/java/com/b2/bookingingorkutek/controller/ReservationPageControllerTest.java
@@ -1,68 +1,66 @@
 package com.b2.bookingingorkutek.controller;
 
-//
-//import com.b2.bookingingorkutek.dto.ModelUserDto;
-//import com.b2.bookingingorkutek.service.AuthorizationService;
-//import com.b2.bookingingorkutek.service.JwtService;
-//import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.*;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.web.client.RestTemplate;
-//
-//import static org.mockito.Mockito.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest(controllers = ReservationPageController.class)
-//@AutoConfigureMockMvc(addFilters = false)
+import com.b2.bookingingorkutek.dto.ModelUserDto;
+import com.b2.bookingingorkutek.service.AuthorizationService;
+import com.b2.bookingingorkutek.service.JwtService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ReservationPageController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ReservationPageControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private JwtService jwtService;
+    private ModelUserDto modelUserDto;
+    @MockBean
+    private AuthorizationService authorizationService;
+    @MockBean
+    private RestTemplate restTemplate;
+    @BeforeEach
+    void setup(){
+         modelUserDto = ModelUserDto.builder()
+                 .emailUser("test@email")
+                 .firstname("test")
+                 .lastname("test")
+                 .role("USER")
+                 .build();
+    }
     @Test
-    void test(){}
-//    @Autowired
-//    private MockMvc mvc;
-//    @MockBean
-//    private JwtService jwtService;
-//    private ModelUserDto modelUserDto;
-//    @MockBean
-//    private AuthorizationService authorizationService;
-//    @MockBean
-//    private RestTemplate restTemplate;
-//    @BeforeEach
-//    void setup(){
-//         modelUserDto = ModelUserDto.builder()
-//                 .emailUser("test@email")
-//                 .firstname("test")
-//                 .lastname("test")
-//                 .role("USER")
-//                 .build();
-//    }
-//    @Test
-//    void testAccessingCreateReservationPageWithRoleUser() throws Exception{
-//        when(authorizationService.requestCurrentUser(anyString())).thenReturn(modelUserDto);
-//        mvc.perform(get("/reservation-page/create"))
-//                .andExpect(status().isOk())
-//                .andExpect(handler().methodName("createReservation"));
-//    }
-//    @Test
-//    void testAccessingCreateReservationPageWithoutRoleUser() throws Exception{
-//        ModelUserDto modelUserDto2 = ModelUserDto.builder()
-//                .emailUser("test@email")
-//                .firstname("test")
-//                .lastname("test")
-//                .role("ADMIN")
-//                .build();
-//        when(authorizationService.requestCurrentUser(anyString())).thenReturn(modelUserDto2);
-//        mvc.perform(get("/reservation-page/create"))
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(handler().methodName("createReservation"));
-//    }
+    void testAccessingCreateReservationPageWithRoleUser() throws Exception{
+        when(authorizationService.requestCurrentUser(anyString())).thenReturn(modelUserDto);
+        mvc.perform(get("/reservation-page/create"))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("createReservation"));
+    }
+    @Test
+    void testAccessingCreateReservationPageWithoutRoleUser() throws Exception{
+        ModelUserDto modelUserDto2 = ModelUserDto.builder()
+                .emailUser("test@email")
+                .firstname("test")
+                .lastname("test")
+                .role("ADMIN")
+                .build();
+        when(authorizationService.requestCurrentUser(anyString())).thenReturn(modelUserDto2);
+        mvc.perform(get("/reservation-page/create"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(handler().methodName("createReservation"));
+    }
 
 
 
